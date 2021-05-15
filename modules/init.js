f00e2e3b5451ca99f6a71f7f62360a9a82668564
Plushtoast <janweigmann@web.de>
2021-05-15 10:51:32 +0200

    adjust to new 1.4

--- modules/init.js
+++ modules/init.js
@@ -1,30 +1,34 @@
 Hooks.once("init", function() {
     console.log("Initializing DSA5 introduction module")
-    let moduleName = "dsa5-introduction"
-    game.settings.register(moduleName, "initialized", {
-        name: "Initialization",
+
+    game.settings.register("dsa5-introduction", "firstTimeStart", {
+        name: "firstTimeStart",
+        hint: "firstTimeStart",
         scope: "world",
         config: false,
         default: false,
         type: Boolean
-    });
-
-    game.settings.registerMenu(moduleName, "init-dialog", {
-        name: "DSA5 Initialization",
-        label: game.i18n.localize("initialize"),
-        hint: game.i18n.localize("dsa5-introduction.importHint"),
-        type: InitializerForm,
-        restricted: true
     })
 })
 
 Hooks.once("ready", function() {
-    if (!game.settings.get("dsa5-introduction", "initialized") && game.user.isGM)
-        new InitializerForm().render()
-})
-
-class InitializerForm extends FormApplication {
-    render() {
-        new game.dsa5.apps.DSA5Initializer("DSA5 introduction module Initialization", game.i18n.format("dsa5-introduction.importContent", { defaultText: game.i18n.localize("importDefault") }), "dsa5-introduction", game.i18n.lang).render(true)
+    if (!game.settings.get("dsa5-introduction", "firstTimeStart")) {
+        let msg = game.i18n.localize('dsa5-introduction.welcome')
+        ChatMessage.create(game.dsa5.apps.DSA5_Utility.chatDataSetup(msg))
+        game.settings.set("dsa5-introduction", "firstTimeStart", true)
     }
-}
+    if (game.i18n.lang == "de") {
+        game.dsa5.apps.journalBrowser.adventures.push({
+            id: "Einführungsabenteuer",
+            path: "modules/dsa5-introduction/adventurede.json",
+            visible: false
+        })
+    } else {
+        game.dsa5.apps.journalBrowser.adventures.push({
+            id: "Introduction Adventure",
+            path: "modules/dsa5-introduction/adventureen.json",
+            visible: false
+        })
+    }
+
+})
